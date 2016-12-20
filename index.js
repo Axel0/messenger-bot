b233aa8bf6407c61f3639129e9c7e26f309b473c
WORK <alessandro.carrese@gmail.com>
2016-12-21 00:32:53 +0100

    Hello World

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app=express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use(bodyParser.json());
 
 app.get('/', function(req,res){
