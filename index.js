f7adb87990e4bd45dfdf8a0b17458bc92f4522b3
WORK <alessandro.carrese@gmail.com>
2016-12-21 00:13:43 +0100

    Hello World

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const bodyParser=require('bodyParser');
 const request=require('request');
 const app=express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
 	res.send('Hello, I am a chat bot')
-})
+});
 
 app.get('/webhook', function(req,res){
 	if (req.query['hub.verify_token']=== 'my_name_is_mine'){
 		res.send(req.query['hub_challange'])
 	}
 	else res.send('Error, wrong token')
-})
+});
 
 app.liste(app.get('port'),function(){
 	console.log('running on port', app.get('port'))
-})
+});
