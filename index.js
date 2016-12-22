89a960befb8b01b4f3fcff0b1e53a7d8e7aac2df
WORK <alessandro.carrese@gmail.com>
2016-12-22 11:48:17 +0100

    updated receivMesssage function name to the correct name : receinedMessage

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.post('/webhook/', function(req,res){
           }
           //This part should be handling a postback coming from the structured message defined in sendGenericMessage function //
           else if (event.postback) {
+            console.log("WebHook received a postback event", event);
           receivedPostback(event);   
         }
           else {
